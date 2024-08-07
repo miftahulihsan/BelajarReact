8a76e8a9e6a2cafde3dbfb6d4fa1f557ad608abc
saannns <76205974+miftahulihsan@users.noreply.github.com>
2024-08-07 11:14:13 +0700

    commit-kedua

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   const accordionArray = [
     {
-      title:"First Accordion",
+      title:"First Frist Accordion",
       content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
